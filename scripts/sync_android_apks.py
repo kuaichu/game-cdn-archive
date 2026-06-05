ac001b5548c2b719a155009cc41b4a0154b91403
Yeque <3219355155@qq.com>
2026-06-06 02:09:37 +0800

    Add Honkai Impact 3rd 4.x legacy links

--- scripts/sync_android_apks.py
+++ scripts/sync_android_apks.py
@@ -349,6 +349,13 @@ KNOWN_APKS = [
     },
     {
         "game_id": "bh3",
+        "version": "4.3.0",
+        "channel": "guofu",
+        "url": "https://app.bh3.com/public/Android/20200921-205726-gf_android_ota-versions-v4_3-Rhythms_of_Neon_guofu.apk",
+        "source": "official CDN URL captured manually",
+    },
+    {
+        "game_id": "bh3",
         "version": "1.9.0",
         "channel": "guofu",
         "url": "http://app.bh3.com/public/Android/20171123-android_versions_v1_9_resurrection_of_the_sacramental_guofu.apk",
